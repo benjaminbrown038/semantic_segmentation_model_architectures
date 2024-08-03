6d10b2bdc846e2fe306dc717dc33c4d6f5109c5a
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-08-03 12:30:51 -0400

    Update unet.py

--- unet.py
+++ unet.py
@@ -1,1 +1,50 @@
+def unet(input_shape=(256, 256, 3)):
+    inputs = Input(input_shape)
 
+    # Encoder
+    conv1 = Conv2D(64, (3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D((2, 2))(conv1)
+
+    conv2 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D((2, 2))(conv2)
+
+    conv3 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D((2, 2))(conv3)
+
+    conv4 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv4)
+    pool4 = MaxPooling2D((2, 2))(conv4)
+
+    # Bottleneck
+    conv5 = Conv2D(1024, (3, 3), activation='relu', padding='same')(pool4)
+    conv5 = Conv2D(1024, (3, 3), activation='relu', padding='same')(conv5)
+
+    # Decoder
+    up6 = UpSampling2D((2, 2))(conv5)
+    up6 = concatenate([up6, conv4])
+    conv6 = Conv2D(512, (3, 3), activation='relu', padding='same')(up6)
+    conv6 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv6)
+
+    up7 = UpSampling2D((2, 2))(conv6)
+    up7 = concatenate([up7, conv3])
+    conv7 = Conv2D(256, (3, 3), activation='relu', padding='same')(up7)
+    conv7 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv7)
+
+    up8 = UpSampling2D((2, 2))(conv7)
+    up8 = concatenate([up8, conv2])
+    conv8 = Conv2D(128, (3, 3), activation='relu', padding='same')(up8)
+    conv8 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv8)
+
+    up9 = UpSampling2D((2, 2))(conv8)
+    up9 = concatenate([up9, conv1])
+    conv9 = Conv2D(64, (3, 3), activation='relu', padding='same')(up9)
+    conv9 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv9)
+
+    outputs = Conv2D(1, (1, 1), activation='sigmoid')(conv9)  # Use 'softmax' for multi-class segmentation
+
+    model = Model(inputs=[inputs], outputs=[outputs])
+
+    return model
